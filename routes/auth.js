7eae5dfb889acbd04f14de355cf5f0b8eb81e992
Prem-Kumar19 <premkum1710@gmail.com>
2025-10-02 10:32:06 +0530

    new rest method

--- routes/auth.js
+++ routes/auth.js
@@ -23,7 +23,7 @@ router.post("/forgot-password", async (req, res) => {
 
   
   const baseURL = process.env.BASE_URL
-  const resetUrl = `${baseURL}/reset-password/${token}`;
+  const resetUrl = `${process.env.BASE_URL}/reset-password/${token}`;
   await sendEmail(user.email, "Password Reset", `Click here: ${resetUrl}`);
 
   res.send("Password reset link sent to your email.");
